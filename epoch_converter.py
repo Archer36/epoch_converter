a1ef3f23053444f2cb5f3cb0b0cfe6725932c97f
Archer36 <archer36@gmail.com>
2022-06-08 15:54:38 -0400

    Added support for int and float timestamps as well as milli, micro, and nano second timestmaps.

--- epoch_converter.py
+++ epoch_converter.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
-import datetime
+from datetime import datetime
 
 
-def main():
+def main(timestamp):
+    if "." in timestamp:
+        print("This appears to be a float timestamp.")
+        timestamp = float(timestamp)
+    else:
+        print("This appears to be a integer timestamp.")
+        timestamp = int(timestamp)
+
+    if [arg for arg in (args.ms, args.un, args.ns) if arg is True]:
+        if args.ms:
+            print("Considering timestamp as milliseconds.")
+            timestamp = timestamp / 1e+3
+        elif args.us:
+            print("Considering timestamp as microseconds.")
+            timestamp = timestamp / 1e+6
+        elif args.ns:
+            print("Considering timestamp as nanoseconds.")
+            timestamp = timestamp / 1e+9
+    else:
+        print("Assuming the timestamp is counting seconds.")
+
     print()
-    print(f"Epoch in local time ({datetime.datetime.now().astimezone().tzname()}): {datetime.datetime.fromtimestamp(args.epoch)}")
-    print(f"Epoch in UTC: {datetime.datetime.utcfromtimestamp(args.epoch)}")
+    print()
+    print(f"Epoch in local time ({datetime.now().astimezone().tzname()}): {datetime.fromtimestamp(timestamp)}")
+    print(f"Epoch in UTC: {datetime.utcfromtimestamp(timestamp)}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert epoch timestamps.")
-    parser.add_argument("epoch", type=int, help="An epoch timestamp to covert")
+    parser.add_argument("timestamp", help="An epoch timestamp to covert")
+    parser.add_argument("--ms", action="store_true",
+                        help="Timestamp value is in milliseconds")
+    parser.add_argument("--un", action="store_true",
+                        help="Timestamp value is in microseconds")
+    parser.add_argument("--ns", action="store_true",
+                        help="Timestamp value is in nanoseconds")
 
     args = parser.parse_args()
 
-    main()
+    main(args.timestamp)
